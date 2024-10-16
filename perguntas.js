4d94ae1f6ef21b83d47dc814e02350d62b1608d9
alferesm05 <163531657+alferesm05@users.noreply.github.com>
2024-10-16 09:31:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'piada',
+'qual a fruta que anda de trem?',
+'o kiwiii'
+)
+
+criaCartao(
+    'piada',
+    'vocễ Conhece a piada do pônei',
+    'Pô nei eu'
+)
+
+criaCartao(
+    'piada',
+'por que o livro de matemática parece tão triste',
+'por causa de todos seus problemas'
+)
+
+criaCartao(
+    'piada',
+    'por que o médico está sempre calmo?',
+    'Porque ele tem muitos pacientes'
+    
+)
